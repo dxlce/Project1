08d4b5508972580268416dffda52612691934a39
xiuxxi <44682939+xiuxxi@users.noreply.github.com>
2019-07-08 14:02:36 -0400

    Add files via upload

---
+++ test_function.py
@@ -0,0 +1,29 @@
+import csv
+
+test_list1 = []
+class HeartRate:
+    def __init__(self, test_list):
+      self.test_list = test_list
+
+      test_read = open('filename.csv')
+      csv_read = csv.reader(test_read)
+      for row in csv_read:
+        test_list.append(row[2])
+
+      print test_list
+
+    """def openFile(test_list):
+      print "hi"
+      test_read = open('filename.csv')
+      csv_read = csv.reader(test_read)
+      for row in csv_read:
+        test_list.append((row[2]))
+      
+
+      print (test_list)"""
+
+test_list2 = HeartRate(test_list1)
+test_list2.__init__
+
+
+ 
