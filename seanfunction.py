46d9f6f7c4a4b7d2be95b3e45c9845c3d57e98fc
xiuxxi <44682939+xiuxxi@users.noreply.github.com>
2019-07-10 12:06:10 -0400

    Update seanfunction.py

--- seanfunction.py
+++ seanfunction.py
@@ -1,9 +1,5 @@
 #NOTE: this is in Python 2, not Python 3
 #modified code so it can verify if the file exists or not
-import csv
-import os.path
-from os import path
-
 test_list1 = []
 
 
@@ -48,6 +44,23 @@ class HeartRate:
       ### Sean:
       ### csv.reader() is a  better solution, but I'll do this quickly
       ### Feel free to fix / improve.
+
+      """if (path.exists('calories.csv')):
+          with open('calories.csv', 'r') as file:
+              reader = csv.reader(open('calories.csv'))
+              row1 = next(reader)
+              if (row1 == ['Polar Calories', 'Formula Calories']):
+                  pass
+              else:
+                  with open('calories.csv', 'wb') as csvfile:
+                      filewriter = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting=csv.QUOTE_MINIMAL)
+                      filewriter.writerow(['Polar Calories', 'Formula Calories'])
+
+      else:
+          with open('calories.csv', 'wb')as csvfile:
+              filewriter = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting=csv.QUOTE_MINIMAL)
+              filewriter.writerow(['Polar Calories', 'Formula Calories'])"""
+              
       dataFromFile = open(filename).readlines()
 
       dataKeys = dataFromFile[0].strip().split(',')
@@ -56,7 +69,7 @@ class HeartRate:
       polarPairs = {key:dataValues[i] for i, key in enumerate(dataKeys)}
       weight = float((polarPairs['Weight (kg)']))
       
-      
+      polarCalories = float(polarPairs['Calories'])
       print "Calories burned (polar) = ", polarPairs['Calories']
 
       heartrate = []
@@ -81,10 +94,10 @@ class HeartRate:
                   heartrate.append(row[2])
 
           heartrate = map(int, heartrate)
-          self.averageHeartRate(weight, age, gender)
+          self.averageHeartRate(polarCalories, weight, age, gender)
           
 
-    def averageHeartRate(self, weight, age, gender):
+    def averageHeartRate(self, polarCalories, weight, age, gender):
       ### returns a float to two decimals of the average heart rate during
       ### the effort
       ### how does is compare to the Polar calculated value
@@ -97,51 +110,63 @@ class HeartRate:
 
       average = float(sum1)/divide
       average = round(average, 2)
-      self.caloriesBurned(average, weight, age, gender)
+      self.caloriesBurned(average, polarCalories, weight, age, gender)
 
-    def maxHeartRate(self):
-      ###returns the maximum heart rate 
-      ### call with initialized object
-      heartrate1 = map(int, self.heartrate)
-      maxHR = 0
-
-      for i in range(len(heartrate1)):
-          if heartrate1[i] > maxHR:
-              maxHR = heartrate1[i]
-
-      return maxHR
-
+    def maxHeartRate():
+      ### returns a float to two decimals of the maximum heart rate during
+      ### the effort
+      ### how does is compare to the Polar calculated value
+      pass
 
     def minHeartRate():
-      ### returns the minimum heart rate 
-      ### call with initialized object
-      heartrate1 = map(int, self.heartrate)
-      minHR = 999
-
-      for i in range(len(heartrate1)):
-          if heartrate1[i] < minHR:
-              minHR = heartrate1[i]
-
-      return minHR
+      ### returns a float to two decimals of the minimum heart rate during
+      ### the effort
+      ### how does is compare to the Polar calculated value
+      pass
 
 
-    def caloriesBurned(self, average, weight, age, gender):
+    def caloriesBurned(self, average, polarCalories, weight, age, gender):
       ### returns the number of calories burned durring the effort
       ### here is a simple formula:
       ### Male: Calories/min = (-55.0969 + (0.6309 * Heart Rate) + (0.1988 * Weight) + (0.2017 * Age)) / 4.184
       ### Female: Calories/min = (-20.4022 + (0.4472 * Heart Rate) - (0.1263 * Weight) + (0.074 * Age)) / 4.184 
       ### how does is compare to the Polar calculated value
+
+      writeFile1 = {
+          'Polar Calories' : 'Formula Calories'
+          }
       if (gender.lower() == 'male'):
           heartRate = 0.6309 * average
           weightCal = 0.1988 * (weight)
           caloriesBurned = (-55.0969 + heartRate + weightCal + (0.2017 * float(age)))
           print "Formula: " + str(caloriesBurned)
 
+          writeFile1.update({str(polarCalories) : str(caloriesBurned)})
+          print writeFile1
+
+          with open('calories.csv', 'wb') as csvfile:
+              filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+              for i in range(len(writeFile1)):
+                  for key in writeFile1:
+                      filewriter.writerow(key)
+                  
+
       elif (gender.lower() == 'female'):
           heartRate = 0.4472 * average
           weightCal = 0.1263 * (weight)
           caloriesBurned = (-20.4022 + heartRate - weightCal + (0.074 * float(age)))
           print "Formula: " + str(caloriesBurned)
+
+          writeFile1.update({str(polarCalories) : str(caloriesBurned)})
+          print writeFile1
+
+          with open('calories.csv', 'wb') as csvfile:
+              filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+              for i in range(len(writeFile1)):
+                  for key in writeFile1:
+                      filewriter.writerow(key)
 
 #asks for user age, gender, and filename (they need to put in the filetype (ie. (name).csv))
 age = (raw_input("Please enter your age: "))
@@ -150,4 +175,5 @@ filename = (raw_input("Please enter the filename: "))
 file_input_prompt = "File does not exist, please re-enter file name. Type 'q' to exit: "
 
 filename = checkFile(filename)
+          
 calorieCount = HeartRate(age, gender, filename)
