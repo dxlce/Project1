9153b2b7b703cca0398c0dfc9a915e3d41801910
xiuxxi <44682939+xiuxxi@users.noreply.github.com>
2019-07-15 15:29:43 -0400

    Add files via upload

--- seanfunction_Laura.py
+++ seanfunction_Laura.py
@@ -66,6 +66,21 @@ class HeartRate:
       
       polarCalories = float(polarPairs['Calories'])
       print "Calories burned (polar) = ", polarPairs['Calories']
+      
+      my_time = polarPairs['Duration']
+
+      """
+      factors = (60, 1, 1/60)
+      print zip(map(int, my_time.split(':')), factors))
+      duration = sum(i*j for i, j in zip(map(int, my_time.split(':')), factors))
+      self.duration = duration
+      print duration"""
+
+
+      (h, m, s) = my_time.split(':')
+      result = int(h) * 3600 + int(m) * 60 + int(s)
+      result2 = float(result)/60
+      self.result2 = result2
 
       heartrate = []
       self.heartrate = heartrate
@@ -89,10 +104,10 @@ class HeartRate:
                   heartrate.append(row[2])
 
           heartrate = map(int, heartrate)
-          self.averageHeartRate(polarCalories, weight, age, gender)
+          self.averageHeartRate(polarCalories, weight, age, gender, result2)
           
 
-    def averageHeartRate(self, polarCalories, weight, age, gender):
+    def averageHeartRate(self, polarCalories, weight, age, gender, result2):
       ### returns a float to two decimals of the average heart rate during
       ### the effort
       ### how does is compare to the Polar calculated value
@@ -115,7 +130,7 @@ class HeartRate:
           average = float(sum1)/divide
           average = round(average, 2)
           print average
-          self.caloriesBurned(average, polarCalories, weight, age, gender)
+          self.caloriesBurned(average, polarCalories, weight, age, gender, result2)
           
       else:
           print ("Invalid range of heartrate.")
@@ -133,7 +148,7 @@ class HeartRate:
       pass
 
 
-    def caloriesBurned(self, average, polarCalories, weight, age, gender):
+    def caloriesBurned(self, average, polarCalories, weight, age, gender, result2):
       ### returns the number of calories burned durring the effort
       ### here is a simple formula:
       ### Male: Calories/min = (-55.0969 + (0.6309 * Heart Rate) + (0.1988 * Weight) + (0.2017 * Age)) / 4.184
@@ -143,34 +158,22 @@ class HeartRate:
       if (gender.lower() == 'male'):
           heartRate = 0.6309 * average
           weightCal = 0.1988 * (weight)
-          caloriesBurned = (-55.0969 + heartRate + weightCal + (0.2017 * float(age)))
+          caloriesBurned = ((-55.0969 + heartRate + weightCal + (0.2017 * float(age))) * result2)/4.184
           print "Formula: " + str(caloriesBurned)
 
-          if (path.exists("calories.txt")):
-              f = open("calories.txt", "a+")
-              f.write(str(average) + " " + str(polarCalories))
-
-          else:
-              f = open("calories.txt", "w+")
-              f.write(str(average) + " " + str(polarCalories))
+          with open('calories.csv', 'ab') as csvfile:
+              filewriter = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting=csv.QUOTE_MINIMAL)
+              filewriter.writerow([str(polarCalories), str(caloriesBurned)])
 
       elif (gender.lower() == 'female'):
           heartRate = 0.4472 * average
           weightCal = 0.1263 * (weight)
-          caloriesBurned = (-20.4022 + heartRate - weightCal + (0.074 * float(age)))
+          caloriesBurned = (-20.4022 + heartRate - weightCal + (0.074 * float(age))) * result2
           print "Formula: " + str(caloriesBurned)
 
-          if(path.exists("calories.txt")):
-              f = open("calories.txt", "a+")
-              f.write(str(average) + " " + str(polarCalories))
-              file.write("\n")
-              
-
-          else:
-              f = open("calories.txt", "w+")
-              f.write(str(average) + " " + str(polarCalories))
-              file.write("\n")
-              
+          with open('calories.csv', 'ab') as csvfile:
+              filewriter = csv.writer(csvfile, delimiter = ',', quotechar = '|', quoting=csv.QUOTE_MINIMAL)
+              filewriter.writerow([str(polarCalories), str(caloriesBurned)])
 
 age = (raw_input("Please enter your age: "))
 gender = (raw_input("Please enter your gender: "))
