4c2c792f2002b5df1b03607d98785372efb8624c
Ray Hao <ray.ray.hao@gmail.com>
2019-07-18 14:11:06 -0400

    Create parseCSV.py

---
+++ parseCSV.py
@@ -0,0 +1,29 @@
+import os
+import csv
+
+test_list1 = []
+class HeartRate:
+    def __init__(self, filename='filename.csv'):
+
+        f = open(filename)
+        csv_f = csv.reader(f)
+        next(csv_f)
+        heartrate = []
+
+        for row in csv_f:
+            heartrate.append(row[1])
+
+        print str(len(heartrate))
+
+        for i in range(1, int(len(heartrarte)/3600)+1):
+                for ele in range(0, 3600):
+                    with open("hour" + str(i) + ".csv", 'wb') as csvfile:
+                        filewriter = csv.writer(csvfile, delimiter=',', quotechar="|", quoting=csv.QUOTE_MINIMAL)
+                        filewriter.writerow(heartrate[ele*i]
+          
+"""
+      self.heartrate = []
+      print self.heartrate
+"""
+
+test_list2 = HeartRate(filename='alexHR.csv')
