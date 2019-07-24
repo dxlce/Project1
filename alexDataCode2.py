98ed13c9e4b0b734d717bcea19c29be1a94a005d
Ray Hao <ray.ray.hao@gmail.com>
2019-07-24 15:50:49 -0400

    Update alexDataCode2.py

--- alexDataCode2.py
+++ alexDataCode2.py
@@ -7,22 +7,13 @@ class HeartRate:
 
         f = open(filename)
         csv_f = csv.reader(f)
-        next(csv_f)import datetime
-from time import time
-import os
-import csv
-
-class HeartRate:
-    def __init__(self, filename='filename.csv'):
-
-        f = open(filename)
-        csv_f = csv.reader(f)
         next(csv_f)
         hrTime = []
         hrValue = []
         timeValue = {}
         counter = 0
         hourNum = 0
+        interval = 0
         t1 = ""
         t2 = ""
         dt1 = 0
@@ -39,6 +30,8 @@ class HeartRate:
         for ele in range(0, len(hrTime)):
             timeValue[list1[ele]] = hrValue[ele]
 
+        interval = raw_input("Enter the number of hours you want to group the data in: ")
+
         ###print timeValue['12T20:35:48']
 
         for t in range(0, len(list1)):
@@ -48,8 +41,8 @@ class HeartRate:
             dt1 = datetime.datetime.strptime(t1,'%Y-%m-%dT%H:%M:%SZ')
             dt2 = datetime.datetime.strptime(t2,'%Y-%m-%dT%H:%M:%SZ')
 
-            if (int(dt2.timestamp() - dt1.timestamp()) <= 3600):
-                with open("hour" + str(hourNum) + ".csv", 'a', newline = "") as csvfile:
+            if (int(dt2.timestamp() - dt1.timestamp()) <= int(interval*60)):
+                with open("hour" + str(hourNum) + ".csv", 'a') as csvfile:
                     filewriter = csv.writer(csvfile, delimiter=',', quotechar="|", quoting=csv.QUOTE_MINIMAL)
                     filewriter.writerow([timeValue[t2]])
             else:
