8433f0951aacef5aa51ffaf4fb7edfff7230deac
Ray Hao <ray.ray.hao@gmail.com>
2019-08-06 18:17:32 -0400

    Create extrapolateData.py

---
+++ extrapolateData.py
@@ -0,0 +1,50 @@
+#ray: still needs to be debugged 
+
+import datetime
+from time import time
+import os
+from os import path
+import csv
+
+class HeartRate:
+    def __init__(self, filename):
+
+        file1Date = ''
+        file1Value = ''
+        file2Date = ''
+        file2Value = ''
+        dt1 = 0
+        dt2 = 0
+
+        dataFiles = next(os.walk('.'))[2]
+        del dataFiles[0]
+        del dataFiles[0]
+        print(str(dataFiles))
+
+        for i in range(0, len(dataFiles) - 1):
+
+            with open('hour' + str(i) + '.csv', 'r') as f1:
+                for row in (list(csv.reader(f1))):
+                    file1Date = row[0]
+                    file1Value = row[1]
+
+            with open('hour' + str(i+1) + '.csv', 'r') as f2:
+                for row in (reversed(list(csv.reader(f2)))):
+                    file2Date = row[0]
+                    file2Value = row[1]
+                    
+            dt1 = datetime.datetime.strptime(file1Date,'%Y-%m-%dT%H:%M:%SZ')
+            dt2 = datetime.datetime.strptime(file2Date,'%Y-%m-%dT%H:%M:%SZ')
+
+            #assuming time is plotted on x-axis and hr on y-axis
+
+            slope = int((int(file2Value) - int(file1Value))/ dt2.timestamp()-dt1.timestamp())
+            intercept = int(dt2.timestamp()/slope/int(file2Value))
+                    
+            with open('extrapolate_' + str(i) + '_' + str(i+1), 'w') as csvfile:
+                filewriter = csv.writer(csvfile, delimiter=',', quotechar="|", quoting=csv.QUOTE_MINIMAL)
+                for m in range(0, int(dt2.timestamp() - dt1.timestamp())):
+                    filewriter.writerow([int(slope)*int(m) + intercept])
+
+            
+test_list2 = HeartRate(filename ='alexData.csv')
